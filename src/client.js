9bd7a3279f2b16fc6cd36763d1a317ad255a1d79
Marian Boda <marianboda@gmail.com>
2015-10-05 01:58:05 +0200

    Added Redux to the stack

--- src/client.js
+++ src/client.js
@@ -1,9 +1,13 @@
 var React = require('react')
+import store from './store'
 
 class Hello extends React.Component {
   render() {
-    return <div>Hello {this.props.name}</div>
+    return <div>Hello {this.props.name} {store.getState()}</div>
   }
 }
+
+
+
 
 React.render(<Hello name="Svet"/>, document.getElementById('content'))
