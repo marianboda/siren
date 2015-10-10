56edfd01e3c61d2c34b0649444d4fecc6ec2aeec
Marian Boda <marianboda@gmail.com>
2015-10-10 21:56:42 +0200

    Cleaned up client index

--- src/client.js
+++ src/client.js
@@ -1,11 +1,7 @@
 'use strict'
-var React = require('react')
-var ReactDom = require('react-dom')
+import React from 'react'
+import ReactDom from 'react-dom'
 
-import * as Actions from './actions'
-import store from './store'
 import {App} from './components/App'
-
-console.log(App)
 
 ReactDom.render(<App/>, document.getElementById('content'))
