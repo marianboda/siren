5f535d2bf11ae0773b6f26a5be32992d0ab46fb1
Marian Boda <marianboda@gmail.com>
2016-04-04 01:34:16 +0200

    Added script for reseting db

--- scripts/db.sql
+++ scripts/db.sql
@@ -1,2 +1,6 @@
+DROP TABLE IF EXISTS scrapes;
+DROP TABLE IF EXISTS posts;
 CREATE TABLE scrapes (id INTEGER PRIMARY KEY UNIQUE NOT NULL, webpage TEXT, lastpage INT) WITHOUT ROWID;
 CREATE TABLE posts (id INTEGER PRIMARY KEY ASC, title TEXT, content TEXT, date TEXT, link TEXT, postid TEXT WITHOUT ROWID);
+
+INSERT INTO scrapes (id, webpage) VALUES (1, "thesirenssound");
