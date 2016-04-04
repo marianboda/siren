6ed77ab1f27d0403e33a913f0c8cd33b2ccadfd2
Marian Boda <marianboda@gmail.com>
2016-04-04 12:21:48 +0200

    Added date to collected values

--- src/services/SirensSound.js
+++ src/services/SirensSound.js
@@ -1,12 +1,14 @@
 request = require('request')
 cheerio = require('cheerio')
 entities = require('entities')
+moment = require('moment')
 
 module.exports = {
   getPost(page) {
     var url = 'http://www.thesirenssound.com/'
     if (page > 1)
       url += `page/${page}/`
+    console.log(url)
     var promise = new Promise( (resolve, reject) => {
       request(url, (err, response, html) => {
         var a = []
@@ -19,6 +21,10 @@ module.exports = {
             post.title = entities.decodeHTML($('h2 a',v).html())
             post.link = $('h2 a',v).attr('href')
             post.content = entities.decodeHTML($(v).html())
+            var rawDateContainer = entities.decodeHTML($('p.info',v).html())
+            var rawDate = rawDateContainer.substr(0,rawDateContainer.indexOf('/')-1)
+            post.date = moment(rawDate, 'MMMM Do, YYYY').format('YYYY-MM-DD')
+            console.log(post.date)
             return post
           })
           resolve(a.toArray())
@@ -26,7 +32,6 @@ module.exports = {
         }
         reject()
         return null
-
       })
     })
     return promise
